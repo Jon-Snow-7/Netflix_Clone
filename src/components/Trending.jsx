10249ff927c77b3f1087a6e94a928b063a484694
Kunal Pal <kunal.pal@nobroker.in>
2025-05-27 11:13:41 +0530

    Trending component on home page

---
+++ src/components/Trending.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import HorizontalScroller from './HorizontalScroller';
+
+const Trending = () => {
+      const [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=20ac0341ec5b2096d68f9c473d7b5d69`
+      );
+      setTrending(res.data.results.slice(6, 100)); // Demo Continue Watching
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <HorizontalScroller title="Trending">
+      {trending.map((movie) => (
+        <div key={movie.id} className="min-w-[180px]">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className="rounded-md w-full h-60 object-cover"
+          />
+          <p className="mt-2 text-sm text-white">{movie.title}</p>
+          <br></br>
+        <br></br>
+        </div>
+        
+      ))}
+    </HorizontalScroller>
+
+  )
+}
+
+export default Trending
