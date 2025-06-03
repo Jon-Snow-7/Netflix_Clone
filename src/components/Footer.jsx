80b587b39a536a825a799b9e0cee272d81fbf463
Kunal Pal <kunal.pal@nobroker.in>
2025-06-04 01:32:51 +0530

    color update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,80 +1,116 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-      
-
-<footer class="bg-gray-900 mt-10 w-[100%]">
-    <div class="mx-auto w-full max-w-screen-xl">
-      <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+      <footer class="bg-gray-900 mt-10 w-[100%] !text-white">
+        <div class="mx-auto w-full max-w-screen-xl">
+          <div class="flex flex-wrap justify-between gap-16 px-4 py-6 lg:py-8">
+            <div className="">
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Company
+              </h2>
+              <ul class="font-medium">
                 <li class="mb-4">
-                    <a href="#" class=" hover:underline">About</a>
+                  <a href="#" class=" hover:underline !text-gray-500 ">
+                    About
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Careers</a>
+                  <a href="#" class="hover:underline !text-gray-500 ">
+                    Careers
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Brand Center</a>
+                  <a href="#" class="hover:underline !text-gray-500 ">
+                    Brand Center
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Blog</a>
+                  <a href="#" class="hover:underline !text-gray-500 ">
+                    Blog
+                  </a>
                 </li>
-            </ul>
-        </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Help center</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+              </ul>
+            </div>
+            <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Help center
+              </h2>
+              <ul class="text-gray-500 dark:text-gray-400 font-medium">
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Discord Server</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Twitter</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Facebook</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Contact Us</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Privacy Policy</a>
+                  <a href="#" class="hover:underline !text-gray-500 ">
+                    Discord Server
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Licensing</a>
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Twitter
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">iOS</a>
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Facebook
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Android</a>
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Legal
+              </h2>
+              <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                <li class="mb-4">
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Privacy Policy
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Windows</a>
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Licensing
+                  </a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">MacOS</a>
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Terms &amp; Conditions
+                  </a>
                 </li>
-            </ul>
-        </div>
-    </div>
-    {/* <div class="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+              </ul>
+            </div>
+            <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Download
+              </h2>
+              <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                <li class="mb-4">
+                  <a href="#" class="hover:underline !text-gray-500">
+                    iOS
+                  </a>
+                </li>
+                <li class="mb-4">
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Android
+                  </a>
+                </li>
+                <li class="mb-4">
+                  <a href="#" class="hover:underline !text-gray-500">
+                    Windows
+                  </a>
+                </li>
+                <li class="mb-4">
+                  <a href="#" class="hover:underline !text-gray-500">
+                    MacOS
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* <div class="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights Reserved.
         </span>
         <div class="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
@@ -110,11 +146,10 @@ const Footer = () => {
               </a>
         </div>
       </div> */}
+        </div>
+      </footer>
     </div>
-</footer>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Footer
+export default Footer;
