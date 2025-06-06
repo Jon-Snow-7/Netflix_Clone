73f13d796ea165e4b4b0585051374267cf6217c6
devjyoti-nobroker <dev.jyoti@nobroker.in>
2025-06-06 14:46:53 +0530

    test page bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import SearchPage from "./pages/SearchPage";
 import WatchHistory from "./pages/WatchHistory";
 import MovieDetail from "./components/MovieDetail";
 import GenrePage from "./pages/Genre";
-import TestPage from "./pages/TestPage";
 
 // import { useDispatch } from "react-redux";
 // import { decrement, increment } from "./components/CounterSlice";
