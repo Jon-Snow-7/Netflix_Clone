c41219af1257cd68678bef9e38bd48f96c549bcb
Kunal Pal <kunal.pal@nobroker.in>
2025-06-11 14:36:49 +0530

    bug fix

--- src/components/Homecom/LatestRelease.jsx
+++ src/components/Homecom/LatestRelease.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, lazy, Suspense } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { latestMovieData } from "../../redux/slice/latestmovieSlice";
 import { useInView } from "react-intersection-observer";
-import MovieRow from "../Rows/MovieRow";
 import { latestMovies } from "../../redux/apis";
 
 // Lazy load MovieRow
@@ -12,6 +11,7 @@ const LatestRelease = ({ className }) => {
   const dispatch = useDispatch();
   const latestState = useSelector((state) => state.latest);
   let latestMovie = latestState?.data?.content || [];
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.1 });
 
   useEffect(() => {
     if(!latestMovies || latestMovie.length==0 )dispatch(latestMovieData());
